d6bcf4913dc50b415a4e3b8f0b9fe5010e17f04a
amandazimmdavis <amanda.s.zimmerman@gmail.com>
2020-11-14 10:24:12 -0600

    initial

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,47 @@
 jQuery(function() {
 
-	$('#my_button').click(function () {
+	//let dog_out = true;
+
+	$('#catbutton').click(function() {
+		$('#catdog').addClass('imageinitial');
+		$('#catdog').addClass("catimage").attr('src', 'images/nigeluse.png');
+		$('#catdog').removeClass('imageinitial');
+		$('#fuzzyfriend').removeClass('imageinitial');
+		$('#yesno').removeClass('imageinitial');
+		$('#youTubeGift').attr('src','https://www.youtube.com/embed/xbs7FT7dXYc');
+		$('#exampleModalLabel').html('Cat treat');
+
 	});
+	$('#dogbutton').click(function() {
+		$('#catdog').addClass('imageinitial');
+		$('#catdog').removeClass("catimage").attr('src', 'images/huskyuse2.png');
+		$('#catdog').removeClass('imageinitial');
+		$('#fuzzyfriend').removeClass('imageinitial');
+		$('#yesno').removeClass('imageinitial');
+		$('#youTubeGift').attr('src','https://www.youtube.com/embed/TVPAUzoYmyU');
+		$('#exampleModalLabel').html('Dog treat');
+
+	});
+
+	$('#yesdog').click(function() {
+
+		$(this).prop('disabled', true);
+		$('#nodog').prop('disabled', true);
+		$('#giveimage').fadeOut('slow', function() {
+			$('#giveimage').attr('src','images/giftopen.png');
+		});
+		$('#giveimage').fadeIn('slow');
+		$('#getGift').removeClass('imageinitial');
+	});
+
+	$('#nodog').click(function() {
+		$(this).prop('disabled', true);
+		$('#yesdog').prop('disabled', true);
+		$('#giveimage').fadeOut('slow', function() {
+			$('#giveimage').attr('src','images/fire.png');
+		});
+		$('#giveimage').fadeIn('slow');
+	});
+
 
 });
